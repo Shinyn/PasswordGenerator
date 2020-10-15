0084aa63ac74f0b959a3192e27d30e29830cbf0e
Shinyn <mattias.alm@hotmail.com>
2020-10-15 06:30:56 +0200

    Converting non-UI stuff to UI for easier resizing + started with design

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class FirstScript : MonoBehaviour
 {
     List<string> passwords = new List<string>();
-    public TextMeshPro tmPro;
+    public TextMeshProUGUI tmProUI;
     public Slider slider;
 
     private void Start()
@@ -18,7 +18,7 @@ public class FirstScript : MonoBehaviour
         slider.maxValue = 128;
     }
 
-    void SliderChanged() // Ändrar tmPro till lösenordet som randomiseras fram när man flyttar slidern vid varje förflyttning
+    void SliderChanged() // Ändrar tmProUI till lösenordet som randomiseras fram när man flyttar slidern vid varje förflyttning
     {
         string password = "";
         int passwordLength = (int)slider.value;
@@ -29,8 +29,8 @@ public class FirstScript : MonoBehaviour
             password = password + c;
         }
 
-        //passwords.Add(password);
-        tmPro.SetText("New password: " + password);
+        passwords.Add(password);
+        tmProUI.SetText("New password: " + password);
     }
 
     void Update()
@@ -51,7 +51,7 @@ public class FirstScript : MonoBehaviour
                 passwords.RemoveAt(i);
             }
 
-            tmPro.SetText("Cleared all passwords.");
+            tmProUI.SetText("Cleared all passwords.");
         }
     }
 
@@ -60,12 +60,12 @@ public class FirstScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && passwords.Count > 0)
         {
             int randomInt = Random.Range(0, passwords.Count);
-            tmPro.SetText("The Devil says: " + passwords[randomInt]);
+            tmProUI.SetText("The Devil says: " + passwords[randomInt]);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && passwords.Count == 0)
         {
-            tmPro.SetText("The Devil is silent...");
+            tmProUI.SetText("The Devil is silent...");
         }
     }
     
@@ -76,16 +76,16 @@ public class FirstScript : MonoBehaviour
         {
             // Man plockar ut det som ligger i passwords och lägger det i password (som är tillfällig för loopen)
             // För varje grej vi plockar ut ur passwords och lägger i password så gör vi detta (Debug.Log just nu).
-            // Du fattar.. 
+            // Du fattar.. man behöver bara ta samma variabeltyp som passwords innehåller
             if (passwords.Count == 0)
             {
-                tmPro.SetText("You have no saved passwords.");
+                tmProUI.SetText("You have no saved passwords.");
             }
 
             foreach (string password in passwords)
             {
                 tmp = tmp + password + "\n";
-                tmPro.SetText("These are your saved passwords: \n" + tmp);
+                tmProUI.SetText("These are your saved passwords: \n" + tmp);
             }
         }
     }
@@ -104,7 +104,7 @@ public class FirstScript : MonoBehaviour
             }
 
             passwords.Add(password);
-            tmPro.SetText("New password: " + password);
+            tmProUI.SetText("New password: " + password);
         }
     }
 
@@ -120,16 +120,7 @@ public class FirstScript : MonoBehaviour
         }
 
         passwords.Add(password);
-        tmPro.SetText("New password: " + password);
-    }
-
-    void RandomTouchPassword()
-    {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            // Sätt den på slidern så det fungerar på mobil?
-        }
+        tmProUI.SetText("New password: " + password);
     }
 
     public void CopyToClipboard()
@@ -141,7 +132,7 @@ public class FirstScript : MonoBehaviour
             foreach (string password in passwords)
             {
                 tmp = tmp + password + "\n";
-                tmPro.SetText("Copied to Clipboard");
+                tmProUI.SetText("Copied to Clipboard");
             }
 
             tmp.CopyToClipboard();

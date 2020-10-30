54d80689a07b11a9d8b2d1d8e2a7907e34580842
Shinyn <mattias.alm@hotmail.com>
2020-10-30 10:13:59 +0100

    Project is done for now, removed the last unnecessary code

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -14,41 +14,19 @@ public class FirstScript : MonoBehaviour
     bool showingMessage = false;
     float messageCooldown = 2f;
 
-    // Appvision: Den ultimata randomiseraren. Randomisera ALLT!... eller ultimata inspirationen för kreativa personer. :D
-
-    // Lägg till fler alternativ till alla. Importera bibliotek som täcker varje kategori.
-    string[] materials = {"Plastic", "Wood", "Metal", "Edible", "Organic", "Paper", "Concrete", "Sand", "Ceramic", "Textile"};
-    string[] scale = {"Giant", "Mini", "Pocket", "Portable", "Wearable", "Inhabitable"};
-    string[] device = {"Robot", "Vehicle", "Computer", "Game", "Tool", "Art"};
-    string[] action = {"Flying", "Random", "Self-Build", "Underwater", "Stealth", "Disposable"};
-    string[] consumer = {"Family", "Personal", "Office", "Home", "Industrial", "Public"};
-    string[] poweredBy = {"Manual", "Electric", "Solar", "Wind", "Water", "Clockwork", "Gas", "Animal"};
-
-    void InspirationGenerator()
-    {
-        int theMaterial = Random.Range(1, materials.Length);
-        int theScale = Random.Range(1, scale.Length);
-        int theDevice = Random.Range(1, device.Length);
-        int theAction = Random.Range(1, action.Length);
-        int theConsumer = Random.Range(1, consumer.Length);
-        int thePoweredBy = Random.Range(1, poweredBy.Length);
-
-        string invention = scale[theScale] + ", " + materials[theMaterial] + ", " + action[theAction] + ", " +
-              poweredBy[thePoweredBy] + ", " +  device[theDevice] + " for the " + consumer[theConsumer];
-
-        Debug.Log("Create a " + invention); // create a: giant, plastic, flying, solar robot for the family
-    }
-
     private void Start()
     {
-        
-        InspirationGenerator();
         slider.onValueChanged.AddListener(delegate { SliderChanged(); });
         Screen.orientation = ScreenOrientation.Portrait;
         slider.minValue = 8;
         slider.maxValue = 128;
         Button clipboardButton = copyToClipBoardButton.GetComponent<Button>();
         clipboardButton.onClick.AddListener(CopyToClipBoardBtn);
+    }
+
+    void Update()
+    {
+        toastUptime();
     }
 
     void SliderChanged() // Ändrar tmProUI till lösenordet som randomiseras fram när man flyttar slidern vid varje förflyttning
@@ -67,7 +45,7 @@ public class FirstScript : MonoBehaviour
         charCounter.SetText("Number of characters: " + slider.value);
     }
 
-    void cooldown()
+    void toastUptime()
     {
         if (showingMessage)
         {
@@ -81,59 +59,6 @@ public class FirstScript : MonoBehaviour
                 showingMessage = false;
                 int password = passwords.Count - 1;
                 tmProUI.SetText("New password: \n\n" + passwords[password]);
-            }
-        }
-    }
-
-    void Update()
-    {
-        cooldown();
-    }
-
-    void ClearPasswordsFromList()
-    {
-        if (Input.GetKeyDown(KeyCode.T) && passwords.Count > 0)
-        {
-            for (int i = passwords.Count - 1; i >= 0; i--)
-            {
-                passwords.RemoveAt(i);
-            }
-
-            tmProUI.SetText("Cleared all passwords.");
-        }
-    }
-
-    void TheDevilSpeaksFromTheVoid() // Error - index out of bound & gör så att Button trycks ner efter nån kombo
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && passwords.Count > 0)
-        {
-            int randomInt = Random.Range(0, passwords.Count);
-            tmProUI.SetText("The Devil says: " + passwords[randomInt]);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && passwords.Count == 0)
-        {
-            tmProUI.SetText("The Devil is silent..." + passwords.Count);
-        }
-    }
-    
-    void ShowPasswords()
-    {
-        string tmp = "";
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            // Man plockar ut det som ligger i passwords och lägger det i password (som är tillfällig för loopen)
-            // För varje grej vi plockar ut ur passwords och lägger i password så gör vi detta (Debug.Log just nu).
-            // Du fattar.. man behöver bara ta samma variabeltyp som passwords innehåller
-            if (passwords.Count == 0)
-            {
-                tmProUI.SetText("You have no saved passwords.");
-            }
-
-            foreach (string password in passwords)
-            {
-                tmp = tmp + password + "\n";
-                tmProUI.SetText("These are your saved passwords: \n" + tmp);
             }
         }
     }
